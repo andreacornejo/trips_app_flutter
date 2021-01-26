10412cffd926b03d3ec1ed66ad2a17dfdad529a0
andreacornejo <andreacornejomoscoso@gmail.com>
2021-01-26 11:29:43 -0400

    Navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:platzi_trips_app/gradient_back.dart';
-import 'package:platzi_trips_app/header_appbar.dart';
-import 'package:platzi_trips_app/review.dart';
-import 'package:platzi_trips_app/review_list.dart';
-import 'description_place.dart';
+import 'package:platzi_trips_app/platzi_trips.dart';
+import 'package:platzi_trips_app/platzi_trips_cupertino.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-    String descriptionDummy = "Lorem ipsum dolor sit amet consectetur adipiscing elit auctor, sapien leo praesent etiam iaculis metus ut, consequat lacinia taciti ultrices at tellus integer. Nulla ad conubia donec senectus netus ultrices semper, metus malesuada ridiculus mollis varius himenaeos tellus, potenti habitasse natoque phasellus integer tristique. \n\nLorem ipsum dolor sit amet consectetur adipiscing elit auctor, sapien leo praesent etiam iaculis metus ut, consequat lacinia taciti ultrices at tellus integer. Nulla ad conubia donec senectus netus ultrices semper, metus malesuada ridiculus mollis varius himenaeos tellus, potenti habitasse natoque phasellus integer tristique.";
+    
 
   @override
   Widget build(BuildContext context) {
@@ -19,21 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        body: Stack(
-          children: <Widget>[
-            ListView(
-              children: [
-                DescriptionPlace("Bahamas",4, descriptionDummy),
-                ReviewList()
-              ],
-            ),
-            HeaderAppBar()   
-          ],
-        ), 
-        //new DescriptionPlace("Bahamas",4, descriptionDummy),
-        
-        ),
+      home: PlatziTripsCupertino()
    );
   }
 }
---
+++ lib/profile_trips.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProfileTrips extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Stack(
+      children: [
+        
+      ],
+    );
+  }
+
+}
